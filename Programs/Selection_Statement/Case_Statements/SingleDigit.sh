7370ffb934772e6f6b953d38747af96c647e40b0
Apoorva1217 <Apoorva1217@gmail.com>
2020-07-16 20:17:38 +0530

    Added:enter single digit and display in word.

---
+++ Programs/Selection_Statement/Case_Statements/SingleDigit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+echo "Enter single digit number:"
+read n
+
+case $n in
+	0)echo "Zero";;
+	1)echo "One";;
+	2)echo "Two";;
+	3)echo "Three";;
+	4)echo "Four";;
+	5)echo "Five";;
+	6)echo "Six";;
+	7)echo "Seven";;
+	8)echo "Eight";;
+	9)echo "Nine";;
+esac
